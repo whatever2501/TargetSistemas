5da69ef403dd7eb173fe934e6c766a7a6d220705
whatever2501 <luiz_nando_vieira@hotmail.com>
2023-04-04 22:19:32 -0300

    FirstCommit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     * @throws IOException
+     * @throws JsonMappingException
+     * @throws JsonParseException
+     */
+    public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+       //Faturamento.start();
+       //Fibonacci.start();
+       //FaturamentoDistribuidora.start();
+       //PalavraInvertida.start();
+    }
+}
